d2f51296c6fb8806e42ed421932af807e2b6facc
Castagner Gabriel <castaggj@mcmaster.ca>
2018-10-21 01:23:53 -0400

    Initial commit

---
+++ BlueDot/Assets/Scripts/BlinkingCursor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+
+public class BlinkingCursor : MonoBehaviour
+{
+
+    private float m_TimeStamp;
+    private bool cursor = false;
+    private string cursorChar = "";
+    private int maxStringLength = 24;
+
+    void update()
+    {
+        if (Time.time - m_TimeStamp >= 0.5)
+        {
+            m_TimeStamp = Time.time;
+            if (cursor == false)
+            {
+                cursor = true;
+                cursorChar += "_";
+            }
+            else
+            {
+                cursor = false;
+                if (cursorChar.Length != 0)
+                {
+                    cursorChar = cursorChar.Substring(0, cursorChar.Length - 1);
+                }
+            }
+            //Text BufferString = GetComponentinChildren<Text>();
+        }
+    }
+}
---
+++ BlueDot/Assets/Scripts/ItemClick.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemClick : MonoBehaviour {
+
+
+	public GameObject bufferfield ;
+
+
+	void Start()
+    {
+    	Button b = GetComponent<Button>();
+    	bufferfield = GameObject.Find("Buffer");
+        b.onClick.AddListener(onClick);
+    }
+
+
+    void onClick()
+    {
+        string keytext = gameObject.GetComponentInChildren<Text>().text;
+        Text bufferfield = GameObject.Find("Buffer").GetComponentInChildren<Text>();
+        bufferfield.text = bufferfield.text + keytext;
+    }
+
+    void onSpace()
+    {
+        Text bufferfield = GameObject.Find("Buffer").GetComponentInChildren<Text>();
+        bufferfield.text = bufferfield.text + " ";
+    }
+
+    void onClear()
+    {
+        Text bufferfield = GameObject.Find("Buffer").GetComponentInChildren<Text>();
+        bufferfield.text = "";
+    }
+
+    void onBackspace()
+    {
+        Text bufferfield = GameObject.Find("Buffer").GetComponentInChildren<Text>();
+        bufferfield.text = bufferfield.text.Substring(0, bufferfield.text.Length-1);
+    }
+
+    void onSubmit(string text = "#BUF")
+    {
+        bool success;
+        Text bufferfield;
+
+        if ("#BUF".Equals(text))
+        {
+            bufferfield = GameObject.Find("Buffer").GetComponentInChildren<Text>();
+            text = bufferfield.text;
+
+        }
+         
+
+        //send text varible to text to speech use success to determine if the text is processed
+
+
+    }
+
+    void onMacro()
+    {
+        Text bufferfield = GameObject.Find("Buffer").GetComponentInChildren<Text>();
+        onSubmit(bufferfield.text);
+
+    }
+
+
+
+}
